836d06100ae1596465fb83c51b63f3271c9fca94
Akash-Kumar-2 <yka334@gmail.com>
2024-03-13 16:18:37 +0530

    Working on upload feature

--- api/index.js
+++ api/index.js
@@ -3,15 +3,23 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const cookieParser = require('cookie-parser');
+const imageDownloader = require('image-downloader');
 const mongoose = require('mongoose');
 const User = require('./models/User.js');
+const multer = require('multer');
+const fs = require('fs');
+
 require('dotenv').config();
 const app = express();
-app.use(express.json());
 
-app.use(cookieParser());
+
 const bcryptSalt = bcrypt.genSaltSync(10);
 const jwtSecret = "vsagfddvsdfj_ofaed_bvfgsbSAFD";
+
+app.use(express.json());
+app.use(cookieParser());
+// to access uploads folder on browser so the we can add photos through link in accomodation
+app.use('/uploads',express.static(__dirname+'/uploads'));
 app.use(cors({
 credentials: true,
 origin:'http://localhost:5173',
@@ -37,7 +45,7 @@ app.post('/register',async (req,res)=>{
         res.status(422).json(e);
     }
    
-})
+});
 
 app.post('/login',async (req,res)=>{
 
@@ -60,7 +68,7 @@ app.post('/login',async (req,res)=>{
         else{
             console.log("Not Found");
         }
-})
+});
 app.get('/profile',(req,res)=>{
     const {token} = req.cookies;
     if(token)
@@ -73,10 +81,33 @@ app.get('/profile',(req,res)=>{
         })
     }else
     res.json(null);
-})
+});
 
 app.post('/logout',(req,res)=>{
     res.cookie('token','').json(true);
-})
+});
 
+app.post('/upload-by-link',async (req,res)=>{
+    const {link} = req.body;
+    const newName ='photo'+ Date.now() + '.jpg';
+    await imageDownloader.image({
+        url: link,
+        dest: __dirname+'/uploads/'+newName,
+    });
+    res.json(newName);
+});
+const photosMiddleware = multer({dest:'uploads/'});
+app.post('/upload',photosMiddleware.array('photos',100),(req,res)=>{
+    const uploadFiles =[];
+    for(let i = 0;i<req.files.length;i++)
+    {
+       const {path,originalname} = req.files[i];
+       const parts = originalname.split('.');
+       const ext = parts[parts.length-1];
+       const newPath = path +'.'+ext;
+       fs.renameSync(path,newPath);
+       uploadFiles.push(newPath.replace("uploads\\",''));
+    }
+res.json(uploadFiles);
+});
 app.listen(4000);
